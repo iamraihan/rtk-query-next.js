9aebd4f5352121a05708bffad6d0c444ed27b2e6
Raihan Ali <trueraihan@gmail.com>
2023-09-29 16:14:39 +0600

    update

--- app/posts/page.js
+++ app/posts/page.js
@@ -5,7 +5,7 @@ import { useGetPostsQuery } from "../redux/services/api/apiSlice";
 const Posts = () => {
   const { data, error, isLoading } = useGetPostsQuery();
   return (
-    <div>
+    <div className="container mx-auto">
       <h2 className="underline">Posts</h2>
       {data?.map((post) => (
         <JsonData data={post} />
