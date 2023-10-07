055dd68aa96da3e28c54f491f559e201361b5f67
Raihan Ali <trueraihan@gmail.com>
2023-10-07 12:54:03 +0600

    remove un used dependency

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,23 @@ module.exports = {
     "./src/**/*.{js,jsx}",
   ],
   theme: {
+    screens: {
+      sm: "640px",
+
+      md: "768px",
+
+      lg: "1024px",
+
+      xl: "1200px",
+    },
+
     container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
+      padding: {
+        DEFAULT: "1rem",
+        sm: "1rem",
+        lg: "1rem",
+        xl: "1rem",
+        "2xl": "1rem",
       },
     },
     extend: {},
